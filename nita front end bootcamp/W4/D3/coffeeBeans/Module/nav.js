a59b3dab15b8d1328041419b67a75148577713cc
asma alashwali <asmaalashwali@hotmail.com>
2023-07-06 09:01:00 +0300

    module demo

---
+++ nita front end bootcamp/W4/D3/coffeeBeans/Module/nav.js
@@ -0,0 +1,24 @@
+export const nav = (
+  <html>
+    <nav class="navParent">
+      <a href="#">
+        <img
+          id="logo"
+          src="./index/c353215aec587b47071b8dafa449362d6e9e47dfa9941f921c5b70e0cbb8c228-removebg-preview 1.png"
+        />
+      </a>
+      <a href="#" class="navItem">
+        Home
+      </a>
+      <a href="#" class="navItem">
+        Shop
+      </a>
+      <a href="#" class="navItem">
+        About Us
+      </a>
+      <a href="contactUs.html" class="navItem">
+        Contact Us
+      </a>
+    </nav>
+  </html>
+);
