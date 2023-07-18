70d0330ce4645a046a04e3638e72c1bd67f5731e
asma alashwali <asmaalashwali@hotmail.com>
2023-07-18 12:04:52 +0300

    assignment6 completed

--- Assignment6.js
+++ Assignment6.js
@@ -1,58 +1,64 @@
-// 1] Write a JavaScript Function to Reverse a String.
+// 1]Function to Reverse a String.
 function reverseString(text) {
-  return text.split('').reverse().join('')
+  return text.split("").reverse().join("");
 }
-console.log(reverseString("asma ahmed"))
+console.log(reverseString("Asma Ahmed"));
 
-// 2] Write a JavaScript Function to check whether a String is Palindrome or not.
-function checkPalindrom (str) {
-  return str == str.split('').reverse().join('');
+// 2]Function to check whether a String is Palindrome or not.
+function checkPalindrome(str) {
+  let StringTLC = str.toLowerCase();
+  return StringTLC == StringTLC.split("").reverse().join("");
 }
-console.log(checkPalindrom('text text'))
+console.log(checkPalindrome("MOm"));
 
-// 3] Write a JavaScript Function to calculate the number of Vowels & Consonants in a String.
-let vowels = ["a", "e", "i", "o", "u"];
+// 3]Function to calculate the number of Vowels & Consonants in a String.
+
 function countVowel(str) {
-  // initialize count
-  let count = 0;
+  let vowels = ["a", "e", "i", "o", "u"];
+  let countVowels = 0;
+  let countConsonants = 0;
   for (let letter of str.toLowerCase()) {
     if (vowels.includes(letter)) {
-      count++;
+      countVowels++;
+    } else if (letter != "") {
+      countConsonants++;
     }
   }
-  return count;
+  return `Vowels Number is ${countVowels}\n Consonants Number is ${countConsonants}`;
 }
+console.log(countVowel("this is Vowels and Consonants Demo"));
+
+// 4]Function to find the common Elements in an Array.
+function CommonItemsArray(arr1, arr2) {
+  var commonArray = [];
+  for (var i = 0; i < arr1.length; i++) {
+    for (var j = 0; j < arr2.length; j++) {
+      if (arr1[i] == arr2[j]) {
+        commonArray.push(arr1[i]);
+      }
+    }
+  }
+  // to remove duplicates from an array
+  commonArray = [...new Set(commonArray)];
+  return commonArray;
+}
+var array1 = [10, 20, 30, 40, 50];
+var array2 = [20, 30, 60, 70, 90];
 console.log(
-  `Number of Vowels is the String is: ${countVowel("this is a vowels demo")}`
+  `The common elements present in both arrays are: ${CommonItemsArray(
+    array1,
+    array2
+  )}`
 );
 
-// 4] Write a JavaScript Function to find the common Elements in an Array.
-function count(str) {
-  let count = str.trim().length;
-  for (let letter of str.toLowerCase()) {
-    if (vowels.includes(letter) || letter == " ") {
-      count--;
-    }
-  }
-  return count;
-}
-console.log(count("this is a vowels demo"));
-
-// 5] Write a JavaScript Function to check if a String is an Anagram of another String or not.
+// 5]Function to check if a String is an Anagram of another String or not.
 function checkAnagram(a, b) {
-     
-  // Not of same length, can't be Anagram
   if (a.length !== b.length) {
-      return false;
+    return false;
   }
-
-  // Inbuilt functions to rearrange the string
-  var str1 = a.split('').sort().join('');
-  var str2 = b.split('').sort().join('');
-
-  var result = (str1 === str2);
+  var str1 = a.split("").sort().join("");
+  var str2 = b.split("").sort().join("");
+  var result = str1 === str2;
   return result;
 }
-
-// Checking the output
-console.log(checkAnagram('abc', 'cba'));
+console.log(checkAnagram("abc", "cba"));
